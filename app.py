bab97618705d4257389a2437378de78599c39e08
Ave4prezi <91444912+Ave4prezi@users.noreply.github.com>
2026-04-30 23:57:51 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import os
 
 app = Flask(__name__)
 
-file_name = "customers.csv"
+import os
+file_name = os.path.join(os.getcwd(), "customers.csv")
 
 # ===============================
 # CREATE CSV IF IT DOESN'T EXIST
