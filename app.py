8e29663d207b65910b1279e7535156c5c9950497
Ave4prezi <91444912+Ave4prezi@users.noreply.github.com>
2026-05-05 18:34:27 -0500

    Refactor app.py to improve structure and readability

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ demo_row = [
     "EB001",
     "Jordan",
     "email@test.com",
-    "+19382655364,+12566420238",
+    "+19382655364",
     "Child",
     "Autism – Nonverbal",
     "Please stay calm. I may not respond verbally. Call my emergency contact(s) immediately.",
