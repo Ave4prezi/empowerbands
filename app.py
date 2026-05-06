4ad773dfd4ba70759410cb99c26bb4051a6079b3
Ave4prezi <91444912+Ave4prezi@users.noreply.github.com>
2026-05-06 16:29:33 -0500

    Refactor app.py for improved structure and clarity

--- app.py
+++ app.py
@@ -26,9 +26,9 @@ LOGO_URL = "https://i.imgur.com/dE4kSOz.png"
 
 demo_row = [
     "EB001",
-    "Jordan",
+    "Jaden",
     "email@test.com",
-    "+19382655364",
+    "+19382655364",+12566121274,
     "Child",
     "Autism – Nonverbal",
     "Please stay calm. I may not respond verbally. Call my emergency contact(s) immediately.",
