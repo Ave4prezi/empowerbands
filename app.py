ac5343fb9db7307a98e5bbd49c574355628ab0fb
Avery Johnson <91444912+Ave4prezi@users.noreply.github.com>
2026-07-14 11:05:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, send_file, jsonify
+from flask import render_template, redirect, url_for request, redirect, session, send_file, jsonify
 import hmac
 import hashlib
 from twilio.rest import Client
